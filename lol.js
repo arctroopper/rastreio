6bd24f79dba131b6a919e2d8b760a1891887f596
arctroopper <arctroopper@hotmail.com>
2023-08-15 00:22:44 -0300

    Create lol.js

---
+++ lol.js
@@ -0,0 +1,209 @@
+'use strict';
+const _0x6cfc = [
+  
+  "status", 
+  "#container-ninja", 
+  "359695AYDYnu", 
+  "GET", 
+  "", 
+  "Seu pedido está á caminho, em breve traremos mais atualizações.", 
+  "attachShadow", 
+  "container-ninja", 
+  "<style> @-webkit-keyframes rotate-forever { 0% { -webkit-transform: rotate(0deg); -moz-transform: rotate(0deg); -ms-transform: rotate(0deg); -o-transform: rotate(0deg); transform: rotate(0deg); } 100% { -webkit-transform: rotate(360deg); -moz-transform: rotate(360deg); -ms-transform: rotate(360deg); -o-transform: rotate(360deg); transform: rotate(360deg); } } @-moz-keyframes rotate-forever { 0% { -webkit-transform: rotate(0deg); -moz-transform: rotate(0deg); -ms-transform: rotate(0deg); -o-transform: rotate(0deg); transform: rotate(0deg); } 100% { -webkit-transform: rotate(360deg); -moz-transform: rotate(360deg); -ms-transform: rotate(360deg); -o-transform: rotate(360deg); transform: rotate(360deg); } } @keyframes rotate-forever { 0% { -webkit-transform: rotate(0deg); -moz-transform: rotate(0deg); -ms-transform: rotate(0deg); -o-transform: rotate(0deg); transform: rotate(0deg); } 100% { -webkit-transform: rotate(360deg); -moz-transform: rotate(360deg); -ms-transform: rotate(360deg); -o-transform: rotate(360deg); transform: rotate(360deg); } } .loading-spinner { -webkit-animation-duration: 1s; -moz-animation-duration: 1s; animation-duration: 1s; -webkit-animation-iteration-count: infinite; -moz-animation-iteration-count: infinite; animation-iteration-count: infinite; -webkit-animation-name: rotate-forever; -moz-animation-name: rotate-forever; animation-name: rotate-forever; -webkit-animation-timing-function: linear; -moz-animation-timing-function: linear; animation-timing-function: linear; height: 30px; width: 30px; border: 2px solid var(--main-color); border-right-color: #fff; border-radius: 50%; } .loading-spinner { transform: translateX(-50%); margin-left: auto; margin-right: auto; } .display-rn-7 { color: var(--title-color); } #loading { display: inline-block; border: 3px solid rgba(255,255,255,.3); border-radius: 50%; border-top-color: #fff; animation: spin 1s ease-in-out infinite; -webkit-animation: spin 1s ease-in-out infinite; } @keyframes spin { to { -webkit-transform: rotate(360deg); } } @-webkit-keyframes spin { to { -webkit-transform: rotate(360deg); } } </style>",
+  "getUTCDate", '<link rel="stylesheet" href="https://rastreioninja.neocities.org/style.css"/><link rel="stylesheet" href="https://rastreioninja.neocities.org/timeline.css"/>', '<h3 class="display-rn-5 text-center-rn" style="margin-top: 10px;margin-bottom: 0;"><span class="badge-rn badge-rn-lg badge-rn-rounded-circle badge-rn-primary">', '<div id="loading"></div>', 
+  "history", "15PBJbvH", '<div class="loading-spinner"></div>', 
+  "</li>", "block",
+  "</div><br><div>", 
+  "length", 
+  "location", 
+  "info", 
+  "map", '<div id="container-ninja" class="container container-ninja" style="display:none;"> <div class="row-rn align-items-center-rn" style="margin-top: 3%"> <div class="col-rn-12 col-rn-md-7 col-rn-lg-6 order-md-1 col-rn-centered aos-init aos-animate" data-aos="fade-up"> <div class="form-group text-center-rn text-md-left-rn"> <h3 class="display-rn-7 text-center-rn">FaÃ§a o rastreamento do seu pedido</h3><br> <div class="input-group-rn mb-3-rn"> <input type="text" class="form-control-rn code-value" placeholder="LO235569205CN"> <div class="input-group-rn-append"> <button class="btn-rn btn-rn-primary btn-rn-outline submit-search-code"> Buscar </button> </div> </div> </div> </div> </div> </div>',"code", "value", "shadowRoot", "20px", "23tZeUVE", "type", "</li></ul></section></section>", "toString", "replace", "success", '<li style="opacity: 0.5;font-size: 14px;margin-top: 0vh;">', "77SWMQCH", '</div></div></div></div><div class="card card-border border-primary"></div></div>','<section class="time-line-data"><h3 style="color: var(--title-color) !important;opacity: 0.5;" class="year">', "server error", '  <section id="containerr" class="pt-4"><div id="container-ninja" class="container container-ninja"><div class="row-rn align-items-center-rn"><div class="col-rn-12 col-rn-md-7 col-rn-lg-6 order-md-1 col-rn-centered aos-init aos-animate" data-aos="fade-up"><h1 class="display-rn-4 text-center-rn" style="margin-top: 0;margin-bottom: 0;"><span class="text-primary-rn">Resultado</span></h1>',".submit-search-code", "click", "remove", "classList", "getUTCMonth", "split", "#loading", "pathname", "innerHTML", "32029vWNXpB", 
+  ".code-value", 
+  "</h3><section><ul>", '</div></div></section><section class="pt-4 pt-md-11-rn"></div></section>', 
+  "</li>", 
+  "test", '<div class="container"><br><div class="alert alert-info fade show" role="alert"style="padding: 18px;font-size: 17px;background-color: #21252f;border-color: #21252e;"><span>Devido ao surto de COVID-19, todos os processos de envio nacional e internacional estarÃ£o sujeitos a atrasos.</span></div><div class="item"><div id="timeline-rn"><div>',
+  "#rastreio", 
+  "response", 
+  "send", '<svg style="display: unset!important; position: relative; top: -2px;" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"/></svg></span></h3>',
+  "Aguarde as prÃ³ximas atualizaÃ§Ãµes do rastreio!", 
+  "description", 
+  "getUTCFullYear", '<br><div class="alert alert-warning fade show" role="alert"><strong>Seu pedido foi enviado. </strong> <span>', 
+  "222983CziLBA", 
+  "onload", 
+  "error", 
+  "", 
+  "</li>", 
+  "join", 
+  "addEventListener", 
+  "substr", 
+  "style", 
+  "is-invalid",
+  "getElementById", 
+  "14261EObMBM", 
+  "responseType", 
+  "querySelector", 
+  "105840EQPAZb", 
+  "code not found", 
+  "299PJNDHH", 
+  "open", 
+  "217927BKxVHB", 
+  "<li>"
+  
+  ];
+  
+const _0x56e0 = function(url, whensCollection) {
+  /** @type {number} */
+  url = url - 491;
+  let _0x6cfc9d = _0x6cfc[url];
+  return _0x6cfc9d;
+};
+const _0x47c0ba = _0x56e0;
+(function(data, oldPassword) {
+  const toMonths = _0x56e0;
+  for (; !![];) {
+    try {
+      const userPsd = -parseInt(toMonths(508)) * parseInt(toMonths(529)) + -parseInt(toMonths(562)) + parseInt(toMonths(566)) + parseInt(toMonths(494)) * -parseInt(toMonths(560)) + -parseInt(toMonths(558)) + parseInt(toMonths(544)) + parseInt(toMonths(555)) * parseInt(toMonths(515));
+      if (userPsd === oldPassword) {
+        break;
+      } else {
+        data["push"](data["shift"]());
+      }
+    } catch (_0x357f6a) {
+      data["push"](data["shift"]());
+    }
+  }
+})(_0x6cfc, 615856);
+const hostAssets = _0x47c0ba(574);
+const elementToInject = document[_0x47c0ba(557)](_0x47c0ba(536));
+const elementShadow = elementToInject[_0x47c0ba(570)]({
+  "mode" : _0x47c0ba(561)
+});
+const cssSpinner = _0x47c0ba(572);
+var html = cssSpinner + hostAssets + _0x47c0ba(503);
+/**
+ * @param {string} offset
+ * @return {undefined}
+ */
+function openTracking(offset) {
+  const _appendPX = _0x47c0ba;
+  addSpinner(elementToInject["shadowRoot"][_appendPX(557)](".submit-search-code"), {
+    "height" : _appendPX(507),
+    "width" : _appendPX(507)
+  });
+  /** @type {string} */
+  var str = "aHR0cHM6Ly9yYXN0cmVpby3drW4dqYS9hcGkvdjIvYnVzY2FyLw==";
+  str = str[_appendPX(512)]("3drW4d", "5uaW5");
+  /** @type {string} */
+  str = atob(str);
+  let context = new XMLHttpRequest;
+  /** @type {string} */
+  context[_appendPX(556)] = "json";
+  context["open"](_appendPX(567), str + offset, true);
+  /**
+   * @return {undefined}
+   */
+  context[_appendPX(545)] = function() {
+    const now = _appendPX;
+    let rpm_traffic = context[now(537)];
+    if (rpm_traffic[now(513)]) {
+      renderTracking(rpm_traffic[now(513)]);
+    } else {
+      if (500 == context[now(564)]) {
+        renderTracking({
+          "error" : "server error"
+        });
+      } else {
+        renderTracking({
+          "error" : now(559)
+        });
+      }
+    }
+  };
+  context[_appendPX(538)](null);
+}
+/**
+ * @param {!Object} data
+ * @return {undefined}
+ */
+function renderTracking(data) {
+  const parseInt = _0x47c0ba;
+  if (data[parseInt(546)]) {
+    if (parseInt(559) == data[parseInt(546)]) {
+      data[parseInt(546)] = parseInt(540);
+    } else {
+      if (parseInt(518) == data[parseInt(546)]) {
+        data[parseInt(546)] = parseInt(569);
+      } else {
+        /** @type {string} */
+        data[parseInt(546)] = "O c\u00c3\u00b3digo de rastreio digitado \u00c3\u00a9 inv\u00c3\u00a1lido.";
+      }
+    }
+  }
+  let marginLeft = '<svg style="width: 19px;position: absolute; left: 52px; z-index: 9999999999; top: -44px; color: var(--main-color);" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="home" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path></svg>';
+  let text = parseInt(519) + (() => {
+    return data[parseInt(504)] ? parseInt(491) + data[parseInt(504)] + parseInt(539) : "";
+  })() + parseInt(547) + (() => {
+    return data["error"] ? parseInt(543) + data[parseInt(546)] + '</span>\n                  </div>\n                  <div class="container">\n                    <div class="item">\n                        <div id="timeline-rn">\n                          <div>\n                              ' + marginLeft + '\n                          </div>\n                          <br>\n                        </div>\n                    </div>\n                  </div>\n                  <div class="card card-border border-primary"></div>\n              </div>\n         ' : 
+    parseInt(535) + marginLeft + parseInt(498) + Object["keys"](data["history"])[parseInt(502)](function(i) {
+      const random = parseInt;
+      return random(517) + formatDate(data[random(493)][i][random(501)]["date"]) + random(531) + (() => {
+        return data[random(493)][i]["type"] ? "\n                                        <li>" + data[random(493)][i][random(509)] + random(548) : random(563) + data[random(493)][i][random(541)] + random(496);
+      })() + random(568) + (() => {
+        return data[random(493)][i][random(541)] ? '\n                                        <li style=" color: #352d2d91; font-size: 14px; ">\n                                          ' + data[random(493)][i][random(541)] + random(533) : "";
+      })() + random(514) + data[random(493)][i][random(501)]["hour"] + random(510);
+    })[parseInt(549)]("") + parseInt(516);
+  })() + parseInt(532);
+  elementShadow[parseInt(557)](parseInt(565))["innerHTML"] = text;
+}
+/**
+ * @param {?} array
+ * @param {?} prop
+ * @return {undefined}
+ */
+function addSpinner(array, prop) {
+  const createElement = _0x47c0ba;
+  array[createElement(528)] = createElement(492);
+  Object["assign"](array[createElement(557)](createElement(526))[createElement(552)], prop);
+}
+/**
+ * @param {?} value
+ * @return {?}
+ */
+function formatDate(value) {
+  /**
+   * @param {number} value
+   * @return {?}
+   */
+  function padTwo(value) {
+    return 0 == value ? "01" : value < 10 ? "0" + value : value;
+  }
+  const now = _0x47c0ba;
+  let rpm_traffic = new Date(value);
+  return padTwo(rpm_traffic[now(573)]()) + "/" + padTwo(rpm_traffic[now(524)]() + 1) + "/" + rpm_traffic[now(542)]()[now(511)]()[now(551)](-2);
+}
+elementShadow[_0x47c0ba(528)] = cssSpinner + _0x47c0ba(495), setTimeout(() => {
+  const d3_vendorSymbol = _0x47c0ba;
+  elementShadow[d3_vendorSymbol(528)] = html;
+  setTimeout(() => {
+    const gotoNewOfflinePage = d3_vendorSymbol;
+    elementShadow[gotoNewOfflinePage(554)](gotoNewOfflinePage(571))["style"]["display"] = gotoNewOfflinePage(497);
+  }, 500);
+  elementToInject[d3_vendorSymbol(506)][d3_vendorSymbol(557)](d3_vendorSymbol(520))[d3_vendorSymbol(550)](d3_vendorSymbol(521), function() {
+    const normalizeKey = d3_vendorSymbol;
+    let element = elementToInject[normalizeKey(506)]["querySelector"](normalizeKey(530));
+    if (/^[A-Z]{2}\d{9}[A-Z]{2}$/[normalizeKey(534)](element[normalizeKey(505)][normalizeKey(512)](/\s/g, "")) || /^[A-Z]{4}\d{9}$/["test"](element[normalizeKey(505)][normalizeKey(512)](/\s/g, "")) || /^[A-Z]{5}\d{10}[A-Z]{2}$/[normalizeKey(534)](element["value"]["replace"](/\s/g, ""))) {
+      openTracking(element["value"]);
+    } else {
+      element["classList"]["add"]("is-invalid");
+      setTimeout(function() {
+        const getMatcherName = normalizeKey;
+        element[getMatcherName(523)][getMatcherName(522)](getMatcherName(553));
+      }, 2E3);
+    }
+  });
+  let value = window[d3_vendorSymbol(500)][d3_vendorSymbol(527)][d3_vendorSymbol(525)]("/");
+  value = value[value[d3_vendorSymbol(499)] - 1];
+  if (/^[A-Z]{2}\d{9}[A-Z]{2}$/["test"](value) || /^[A-Z]{4}\d{9}$/[d3_vendorSymbol(534)](value) || /^[A-Z]{5}\d{10}[A-Z]{2}$/[d3_vendorSymbol(534)](value)) {
+    openTracking(value);
+  }
+}, 1500);
